ebc817ed86fd765b6514f7dcc2e16ee4348f37ba
Mathias Ducancel <mathiasduc@gmail.com>
2017-03-01 16:24:41 +0100

    Modify Input component to make it stateless

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -1,19 +1,16 @@
 import React from 'react';
 
-export default class Input extends React.Component{
-	constructor(props){
-		super(props);
-	}
-
-	render(){
-		return(
-			<input name={this.props.name} onChange={this.props.handleInputChange} value={this.props.value} type={this.props.type}/>
-			);
-	}
+const Input = (props)=>{
+	return(
+		<input name={props.name} onChange={props.handleInputChange} value={props.value} type={props.type}/>
+		);
 }
+
 Input.propTypes ={
 	name: React.PropTypes.string.isRequired,
 	handleInputChange: React.PropTypes.func.isRequired,
 	value: React.PropTypes.string.isRequired,
 	type: React.PropTypes.string.isRequired,
 }
+
+export default Input;
--- src/LoggingForm/LoggingForm.js
+++ src/LoggingForm/LoggingForm.js
@@ -5,35 +5,36 @@ export default class LoggingForm extends React.Component{
 	constructor(props){
 		super(props);
 		this.state={
-			logginValue: ''
+			organisation:'',
+			loggin: '',
+			password: ''
 		};
 		this.handleInputChange = this.handleInputChange.bind(this);
 	}
 
 	handleInputChange(event) {
-    const target = event.target;
-    //we test if the input is or not a checkbox for make sure we take the right value
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-    this.setState({
-      [name]: value
-    });
-  }
+		const target = event.target;
+		/*we test if the input is or not a checkbox for making sure we take the right value*/
+		const value = target.type === 'checkbox' ? target.checked : target.value;
+		const name = target.name;
+		this.setState({[name]: value});
+	}
+
 
 	render(){
 		return(
 			<div className="LoggingForm">
 			<form>
-				
-				<label>Votre nom ou identifiant
-					<Input name="loggin" handleInputChange={this.handleInputChange} value={this.state.loggin} type="text"/>				
-				</label>
-				<label>Organisation
-					<Input name="organisation" handleInputChange={this.handleInputChange} value={this.state.organisation} type="text"/>				
-				</label>
-				<label>Mot de passe
-					<Input name="password" handleInputChange={this.handleInputChange} value={this.state.password} type="password"/>				
-				</label>
+
+			<label>Votre nom ou identifiant
+			<Input name="loggin" handleInputChange={this.handleInputChange} value={this.state.loggin} type="text"/>				
+			</label>
+			<label>Organisation
+			<Input name="organisation" handleInputChange={this.handleInputChange} value={this.state.organisation} type="text"/>				
+			</label>
+			<label>Mot de passe
+			<Input name="password" handleInputChange={this.handleInputChange} value={this.state.password} type="password"/>				
+			</label>
 			</form>
 			</div>
 			);
