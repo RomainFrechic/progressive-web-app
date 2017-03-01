a7402be3163252903f79a18ca56cc78962c0c438
Romain <RomainFrechic@outlook.fr>
2017-03-01 16:22:56 +0100

    loggin form will correctly authentify

--- src/LoggingForm/LoggingForm.js
+++ src/LoggingForm/LoggingForm.js
@@ -1,24 +1,45 @@
 import React from 'react';
 import Input from '../Input/Input';
-
+import Button from '../Button/Button';
+import axios from 'axios';
 export default class LoggingForm extends React.Component{
 	constructor(props){
 		super(props);
 		this.state={
-			logginValue: ''
+			organisation:'',
+			loggin:'',
+			password:''
 		};
 		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleClick = this.handleClick.bind(this);
 	}
 
 	handleInputChange(event) {
     const target = event.target;
-    //we test if the input is or not a checkbox for make sure we take the right value
+    //we test if the input is or not a checkbox for making sure we take the right value
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
     this.setState({
       [name]: value
     });
   }
+  
+   handleClick(event) {
+   	event.preventDefault();
+   	const {organisation, loggin, password} = this.state;
+   	console.log(organisation, loggin, password);
+   	axios.post('http://localhost:8000/api/authenticate',{
+   		loggin:loggin,
+   		password:password,
+   		organisation:organisation
+   	})
+   	.then(function(response){
+	console.log(response);
+   	})
+   	.catch(function(error){
+   	console.log(error);
+   	});
+   }
 
 	render(){
 		return(
@@ -34,6 +55,7 @@ export default class LoggingForm extends React.Component{
 				<label>Mot de passe
 					<Input name="password" handleInputChange={this.handleInputChange} value={this.state.password} type="password"/>				
 				</label>
+				<Button className="buttonLoggin" handleClick={this.handleClick}>Se connecter</Button>
 			</form>
 			</div>
 			);
